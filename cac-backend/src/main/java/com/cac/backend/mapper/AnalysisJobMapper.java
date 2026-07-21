d4255cb6c35a4c698929fe893a1e429d813989e1
Nick Zhu <hyong8599@gmail.com>
2026-07-21 14:30:39 +0800

    main

--- cac-backend/src/main/java/com/cac/backend/mapper/AnalysisJobMapper.java
+++
@@ -1,7 +0,0 @@
-package com.cac.backend.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cac.backend.entity.AnalysisJob;
-
-public interface AnalysisJobMapper extends BaseMapper<AnalysisJob> {
-}
--- cac-backend/src/main/java/com/cac/backend/mapper/CacResultMapper.java
+++
@@ -1,7 +0,0 @@
-package com.cac.backend.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cac.backend.entity.CacResult;
-
-public interface CacResultMapper extends BaseMapper<CacResult> {
-}
